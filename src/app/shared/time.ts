a35a0c2fd32c95b1277102f90b3567d21f6a6629
dianalemen <diana.lemen@gmail.com>
2017-03-23 21:23:11 +0200

    delelted

--- src/app/shared/time.ts
+++
@@ -1,9 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'time'})
-export class TimePipe implements PipeTransform{
-    transform(value) {
-         value = new Date;
-         return value.getHours();
-    } 
-}
