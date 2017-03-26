fbc0b334aae7de826d7381a77bf21e0c9c952171
dianalemen <diana.lemen@gmail.com>
2017-03-26 12:36:23 +0300

    auth with google

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+///<reference path="../../../../node_modules/@types/gapi/index.d.ts"/>
+import { Component, OnInit, NgZone } from '@angular/core';
 import { User } from './login.interface';
-import { AuthService } from '../../core/auth.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
-
+import { AuthService } from '../../core/auth.service'
+declare var gapi: any;
 
 @Component({
   selector: 'ct-login',
@@ -13,23 +11,40 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 
 export class LoginComponent implements OnInit {
-  user: User = {
-    email: '',
-    password: ''
-  };
+  profile;
+  username;
 
-  constructor(private authService: AuthService) {
 
-  }
+  constructor(private authService: AuthService,
+              private zone: NgZone) {
 
-  ngOnInit() {
-    
-   
   }
 
   onSubmit(formData){
-    this.authService.login(formData)
-    console.log(formData)
+      this.authService.login(formData);
   }
-  
+    
+  ngOnInit() {
+    gapi.load('auth2', () => {
+      var auth2 = gapi.auth2.init({
+        client_id: '718730295293-06f2hsflisqiteoio1s248phhiontsos.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin'
+      });
+
+      auth2.attachClickHandler(
+        document.getElementById('google-login-btn'), {},
+        this.onSuccess.bind(this),
+        this.onFailure
+      );
+    });
+  }
+
+  onFailure (){}
+
+  onSuccess (user): void {
+    this.zone.run(() => {
+      this.profile = user.getBasicProfile();
+      this.username = user.getBasicProfile().getEmail();
+    });
+  }
 }
