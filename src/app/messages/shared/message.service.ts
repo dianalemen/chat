4c6c736d73300237147fad8020bedce12ea3ca12
dianalemen <diana.lemen@gmail.com>
2017-05-01 22:59:47 +0300

    changed styles

--- src/app/messages/shared/message.service.ts
+++ src/app/messages/shared/message.service.ts
@@ -24,7 +24,7 @@ private heroesUrl = 'http://eleksfrontendcamp-mockapitron.rhcloud.com/messages';
 
     private extractData(res: Response) {
         let body = res.json();
-        return body.slice(body.length-6);
+        return body.slice(body.length-4);
     }
 
    // getAll(chatId){
