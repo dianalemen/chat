17bbd935d21be41d82c2b80cd63e8ebb32ca86d1
dianalemen <diana.lemen@gmail.com>
2017-05-23 14:42:14 +0300

    fixed home component

--- src/app/app-home.component.ts
+++ src/app/app-home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<h1 id="app-home">Welcome to Chat Application</h1>',
+  template: '<section class="home"><h1 id="app-home">Welcome to Chat Application</h1></section>',
    styleUrls: ['./app.component.css']
 })
 
